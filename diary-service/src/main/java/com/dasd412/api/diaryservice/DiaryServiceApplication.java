aafa0d1d3053bc36a76fa666b7fbede98ac81313
dasd <dasd412@naver.com>
2022-11-18 14:54:09 +0900

    msa 프로젝트 초기 세팅 대략 완료.
    [구현 순서]
    1. diary-service 분할 및 도커 말기
    2. config-server 구성 및 도커 말기
    3. writer-service 분할 및 도커 말기
    4. 기존 테스트 코드들 마이그레이션

---
+++ diary-service/src/main/java/com/dasd412/api/diaryservice/DiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dasd412.api.diaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiaryServiceApplication.class, args);
+	}
+
+}
---
+++ diary-service/src/test/java/com/dasd412/api/diaryservice/DiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasd412.api.diaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
