7cfc3d881a5a7c866d1af95b2049318ba1b852da
dasd <dasd412@naver.com>
2022-11-21 10:53:31 +0900

    1. 컨피그 서버 구성 2. 다이어리 서비스와 컨피그 서버 도커 컴포즈로 연결 및 실행 완료

--- diary-service/src/main/java/com/dasd412/api/diaryservice/DiaryServiceApplication.java
+++ diary-service/src/main/java/com/dasd412/api/diaryservice/DiaryServiceApplication.java
@@ -2,8 +2,10 @@ package com.dasd412.api.diaryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@RefreshScope
 @EnableJpaAuditing
 @SpringBootApplication
 public class DiaryServiceApplication {
