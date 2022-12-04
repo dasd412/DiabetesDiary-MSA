24faf7a153b08dd53dc6be335ede079b8dc9bf8d
dasd <dasd412@naver.com>
2022-12-04 10:07:36 +0900

    게이트웨이 서버 구성정보 설정

---
+++ gateway-server/src/main/java/com/dasd412/api/gatewayserver/GatewayServerApplication.java
@@ -0,0 +1,15 @@
+package com.dasd412.api.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServerApplication.class, args);
+	}
+
+}
---
+++ gateway-server/src/test/java/com/dasd412/api/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasd412.api.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
