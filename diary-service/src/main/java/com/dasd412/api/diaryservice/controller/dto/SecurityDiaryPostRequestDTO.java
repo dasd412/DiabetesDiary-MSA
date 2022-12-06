21eeb1d961189ccc4e5277ba86bade76b4942b69
dasd <dasd412@naver.com>
2022-12-06 16:29:38 +0900

    DTO도 하위 엔티티 내용 추가

--- diary-service/src/main/java/com/dasd412/api/diaryservice/controller/dto/SecurityDiaryPostRequestDTO.java
+++ diary-service/src/main/java/com/dasd412/api/diaryservice/controller/dto/SecurityDiaryPostRequestDTO.java
@@ -1,5 +1,6 @@
 package com.dasd412.api.diaryservice.controller.dto;
 
+import com.dasd412.api.diaryservice.controller.FoodListSize;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -15,67 +16,68 @@ import java.util.List;
 @Builder
 @Getter
 public class SecurityDiaryPostRequestDTO {
-    
-    //todo 지워야 할 것 같은 속성
+
+    @NotEmpty
+    @NotNull
     private final Long writerId;
     
     private final int fastingPlasmaGlucose;
 
     private final String remark;
 
-//    @NotEmpty
-//    @NotNull
-//    private final String year;
-//
-//    @NotEmpty
-//    @NotNull
-//    private final String month;
-//
-//    @NotEmpty
-//    @NotNull
-//    private final String day;
-//
-//    @NotEmpty
-//    @NotNull
-//    private final String hour;
-//
-//    @NotEmpty
-//    @NotNull
-//    private final String minute;
-//
-//    @NotEmpty
-//    @NotNull
-//    private final String second;
-//
-//    private final int breakFastSugar;
-//    private final int lunchSugar;
-//    private final int dinnerSugar;
+    @NotEmpty
+    @NotNull
+    private final String year;
 
-//    @Size(max = FoodListSize.FOOD_LIST_SIZE)
-//    private final List<SecurityFoodDTO> breakFastFoods;
-//
-//    @Size(max = FoodListSize.FOOD_LIST_SIZE)
-//    private final List<SecurityFoodDTO> lunchFoods;
-//
-//    @Size(max = FoodListSize.FOOD_LIST_SIZE)
-//    private final List<SecurityFoodDTO> dinnerFoods;
+    @NotEmpty
+    @NotNull
+    private final String month;
 
-//    public String toString() {
-//        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
-//                .append("fpg", fastingPlasmaGlucose)
-//                .append("remark", remark)
-//                .append("year", year)
-//                .append("month", month)
-//                .append("day", day)
-//                .append("hour", hour)
-//                .append("minute", minute)
-//                .append("second", second)
-//                .append("breakFastSugar", breakFastSugar)
-//                .append("lunchSugar", lunchSugar)
-//                .append("dinnerSugar", dinnerSugar)
-////                .append("breakFastFoods", breakFastFoods)
-////                .append("lunchFoods", lunchFoods)
-////                .append("dinnerFoods", dinnerFoods)
-//                .toString();
-//    }
+    @NotEmpty
+    @NotNull
+    private final String day;
+
+    @NotEmpty
+    @NotNull
+    private final String hour;
+
+    @NotEmpty
+    @NotNull
+    private final String minute;
+
+    @NotEmpty
+    @NotNull
+    private final String second;
+
+    private final int breakFastSugar;
+    private final int lunchSugar;
+    private final int dinnerSugar;
+
+    @Size(max = FoodListSize.FOOD_LIST_SIZE)
+    private final List<SecurityFoodDTO> breakFastFoods;
+
+    @Size(max = FoodListSize.FOOD_LIST_SIZE)
+    private final List<SecurityFoodDTO> lunchFoods;
+
+    @Size(max = FoodListSize.FOOD_LIST_SIZE)
+    private final List<SecurityFoodDTO> dinnerFoods;
+
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .append("fpg", fastingPlasmaGlucose)
+                .append("remark", remark)
+                .append("year", year)
+                .append("month", month)
+                .append("day", day)
+                .append("hour", hour)
+                .append("minute", minute)
+                .append("second", second)
+                .append("breakFastSugar", breakFastSugar)
+                .append("lunchSugar", lunchSugar)
+                .append("dinnerSugar", dinnerSugar)
+                .append("breakFastFoods", breakFastFoods)
+                .append("lunchFoods", lunchFoods)
+                .append("dinnerFoods", dinnerFoods)
+                .toString();
+    }
 }
