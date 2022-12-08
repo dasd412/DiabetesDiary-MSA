77c024566564c5185a1a0271c008296436103520
dasd <dasd412@naver.com>
2022-12-08 11:14:13 +0900

    1. 읽기 전용 일지 서비스 프로젝트 추가 2. 리드미 업데이트

---
+++ read-diary-service/src/main/java/com/dasd412/api/readdiaryservice/ReadDiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dasd412.api.readdiaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadDiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadDiaryServiceApplication.class, args);
+	}
+
+}
---
+++ read-diary-service/src/test/java/com/dasd412/api/readdiaryservice/ReadDiaryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dasd412.api.readdiaryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadDiaryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
