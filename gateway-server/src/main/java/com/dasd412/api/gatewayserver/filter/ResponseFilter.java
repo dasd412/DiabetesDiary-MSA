8983cffb40fb011a17152e956c3878d76573110c
dasd <dasd412@naver.com>
2022-12-16 18:03:48 +0900

    1. 집킨 의존성 추가 2. 게이트웨이 바깥으로 나가는 응답에 슬루스 traceId 추가

--- gateway-server/src/main/java/com/dasd412/api/gatewayserver/filter/ResponseFilter.java
+++ gateway-server/src/main/java/com/dasd412/api/gatewayserver/filter/ResponseFilter.java
@@ -1,11 +1,11 @@
 package com.dasd412.api.gatewayserver.filter;
 
+import brave.Tracer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import reactor.core.publisher.Mono;
 
 @Configuration
@@ -14,19 +14,20 @@ public class ResponseFilter {
     private final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
 
     private final FilterUtils filterUtils;
+    private final Tracer tracer;
 
-    public ResponseFilter(FilterUtils filterUtils) {
+    public ResponseFilter(FilterUtils filterUtils, Tracer tracer) {
         this.filterUtils = filterUtils;
+        this.tracer = tracer;
     }
 
     @Bean
     public GlobalFilter postGlobalFilter() {
         return ((exchange, chain) -> {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = String.valueOf(filterUtils.getCorrelationId(requestHeaders));
-                logger.debug("adding correlation id to the outbound headers.{}", correlationId);
-                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+                String traceId=tracer.currentSpan().context().traceIdString();
+                logger.debug("adding correlation id to the outbound headers.{}", traceId);
+                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, traceId);
                 logger.debug("complete outgoing request for {}.", exchange.getRequest().getURI());
             }));
         });
