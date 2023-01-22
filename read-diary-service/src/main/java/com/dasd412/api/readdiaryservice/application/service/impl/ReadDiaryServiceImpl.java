e6c0060ac9629933a8f810f3a004bf2f28379a8b
dasd <dasd412@naver.com>
2023-01-22 12:13:00 +0900

    feat : Add food board logic in service layer

--- read-diary-service/src/main/java/com/dasd412/api/readdiaryservice/application/service/impl/ReadDiaryServiceImpl.java
+++ read-diary-service/src/main/java/com/dasd412/api/readdiaryservice/application/service/impl/ReadDiaryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.dasd412.api.readdiaryservice.application.service.impl;
 
 import com.dasd412.api.readdiaryservice.adapter.in.web.FoodPageVO;
+import com.dasd412.api.readdiaryservice.adapter.in.web.InequalitySign;
 import com.dasd412.api.readdiaryservice.adapter.out.persistence.diary.DiaryDocumentRepository;
 import com.dasd412.api.readdiaryservice.adapter.out.web.dto.AllBloodSugarDTO;
 import com.dasd412.api.readdiaryservice.adapter.out.web.dto.BloodSugarBetweenTimeSpanDTO;
@@ -11,19 +12,24 @@ import com.dasd412.api.readdiaryservice.common.utils.trace.UserContextHolder;
 import com.dasd412.api.readdiaryservice.domain.diary.DiabetesDiaryDocument;
 import com.dasd412.api.readdiaryservice.domain.diary.QDiabetesDiaryDocument;
 import com.dasd412.api.readdiaryservice.domain.diet.DietDocument;
+import com.dasd412.api.readdiaryservice.domain.diet.QDietDocument;
+import com.dasd412.api.readdiaryservice.domain.food.FoodDocument;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.domain.Page;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 
+import static com.dasd412.api.readdiaryservice.common.utils.date.DateStringConverter.isStartDateEqualOrBeforeEndDate;
 import static com.google.common.base.Preconditions.checkArgument;
 
 @Service
@@ -54,9 +60,10 @@ public class ReadDiaryServiceImpl implements ReadDiaryService {
         logger.info("find fpg between time span in ReadDiaryService : {} ", UserContextHolder.getContext().getCorrelationId());
 
         LocalDateTime startDate = DateStringConverter.convertMapParamsToStartDate(timeSpan);
+
         LocalDateTime endDate = DateStringConverter.convertMapParamsToEndDate(timeSpan);
 
-        checkArgument(DateStringConverter.isStartDateEqualOrBeforeEndDate(startDate, endDate), "startDate must be equal or before than endDate");
+        checkArgument(isStartDateEqualOrBeforeEndDate(startDate, endDate), "startDate must be equal or before than endDate");
 
         Predicate predicate = qDocument.writerId.eq(Long.parseLong(writerId))
                 .and(qDocument.writtenTime.between(startDate, endDate));
@@ -90,9 +97,10 @@ public class ReadDiaryServiceImpl implements ReadDiaryService {
         logger.info("find blood sugar between time span in ReadDiaryService : {} ", UserContextHolder.getContext().getCorrelationId());
 
         LocalDateTime startDate = DateStringConverter.convertMapParamsToStartDate(timeSpan);
+
         LocalDateTime endDate = DateStringConverter.convertMapParamsToEndDate(timeSpan);
 
-        checkArgument(DateStringConverter.isStartDateEqualOrBeforeEndDate(startDate, endDate), "startDate must be equal or before than endDate");
+        checkArgument(isStartDateEqualOrBeforeEndDate(startDate, endDate), "startDate must be equal or before than endDate");
 
         Predicate predicate = qDocument.writerId.eq(Long.parseLong(writerId))
                 .and(qDocument.writtenTime.between(startDate, endDate));
@@ -114,7 +122,85 @@ public class ReadDiaryServiceImpl implements ReadDiaryService {
 
     @Override
     public Page<FoodBoardDTO> getFoodByPagination(String writerId, FoodPageVO foodPageVO) {
-        return null;
+        logger.info("find food board in ReadDiaryService : {} ", UserContextHolder.getContext().getCorrelationId());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        if (foodPageVO.getSign() != null && foodPageVO.getEnumOfSign() != InequalitySign.NONE) {
+            booleanBuilder.and(decideEqualitySignOfBloodSugar(foodPageVO.getEnumOfSign(), foodPageVO.getBloodSugar()));
+        }
+
+        LocalDateTime startDate;
+
+        LocalDateTime endDate;
+
+        try {
+            startDate = foodPageVO.convertStartDate().orElseThrow(() -> new DateTimeException("start date cannot convert"));
+
+            endDate = foodPageVO.convertEndDate().orElseThrow(() -> new DateTimeException("end date cannot convert"));
+
+            if (isStartDateEqualOrBeforeEndDate(startDate, endDate)) {
+                booleanBuilder.and(qDocument.writerId.eq(Long.parseLong(writerId))
+                        .and(qDocument.writtenTime.between(startDate, endDate)));
+            }
+        } catch (DateTimeException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+
+        Pageable pageable = makePageableWithSort(foodPageVO);
+
+        List<DiabetesDiaryDocument> diaryDocumentList = (List<DiabetesDiaryDocument>) diaryDocumentRepository.findAll(booleanBuilder.getValue(), pageable);
+
+        List<FoodBoardDTO> dtoList = new ArrayList<>();
+
+        for (DiabetesDiaryDocument diaryDocument : diaryDocumentList) {
+            for (DietDocument dietDocument : diaryDocument.getDietList()) {
+                for (FoodDocument foodDocument : dietDocument.getFoodList()) {
+                    dtoList.add(new FoodBoardDTO(foodDocument.getFoodName(), dietDocument.getBloodSugar(),
+                            diaryDocument.getWrittenTime(), diaryDocument.getDiaryId()
+                    ));
+                }
+            }
+        }
+
+        long count=diaryDocumentRepository.count(booleanBuilder.getValue());
+
+        return new PageImpl<>(dtoList,pageable,count);
     }
 
+    private BooleanBuilder decideEqualitySignOfBloodSugar(InequalitySign sign, int bloodSugar) {
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+
+        switch (sign) {
+            case GREATER:
+                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.gt(bloodSugar));
+                break;
+
+            case LESSER:
+                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.lt(bloodSugar));
+                break;
+
+            case EQUAL:
+                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.eq(bloodSugar));
+                break;
+
+            case GREAT_OR_EQUAL:
+                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.goe(bloodSugar));
+                break;
+
+            case LESSER_OR_EQUAL:
+                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.loe(bloodSugar));
+                break;
+        }
+
+        return booleanBuilder;
+    }
+
+    private Pageable makePageableWithSort(FoodPageVO foodPageVO) {
+        // Sort.by 내 인자는 document 엔티티 내의 @Field 값과 동일해야 한다.
+        Sort sort = Sort.by("blood_sugar").descending()
+                .and(Sort.by("written_time").descending().and(Sort.by("food_name")).ascending());
+
+        return PageRequest.of(foodPageVO.getPage() - 1, foodPageVO.getSize(), sort);
+    }
 }
