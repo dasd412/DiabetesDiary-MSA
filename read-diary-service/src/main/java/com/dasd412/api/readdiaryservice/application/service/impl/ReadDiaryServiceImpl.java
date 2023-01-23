aa60ba17a24803879fb2a7d17607687f0df5b102
dasd <dasd412@naver.com>
2023-01-23 15:06:33 +0900

    feat : Add logic of collecting and filtering list of FoodBoardDTO
    next step is to make this page<T>

--- read-diary-service/src/main/java/com/dasd412/api/readdiaryservice/application/service/impl/ReadDiaryServiceImpl.java
+++ read-diary-service/src/main/java/com/dasd412/api/readdiaryservice/application/service/impl/ReadDiaryServiceImpl.java
@@ -12,9 +12,8 @@ import com.dasd412.api.readdiaryservice.common.utils.trace.UserContextHolder;
 import com.dasd412.api.readdiaryservice.domain.diary.DiabetesDiaryDocument;
 import com.dasd412.api.readdiaryservice.domain.diary.QDiabetesDiaryDocument;
 import com.dasd412.api.readdiaryservice.domain.diet.DietDocument;
-import com.dasd412.api.readdiaryservice.domain.diet.QDietDocument;
 
-import com.querydsl.core.BooleanBuilder;
+import com.dasd412.api.readdiaryservice.domain.food.FoodDocument;
 import com.querydsl.core.types.ExpressionUtils;
 import com.querydsl.core.types.Predicate;
 import org.slf4j.Logger;
@@ -23,7 +22,6 @@ import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.DateTimeException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -125,34 +123,87 @@ public class ReadDiaryServiceImpl implements ReadDiaryService {
     public Page<FoodBoardDTO> getFoodByPagination(String writerId, FoodPageVO foodPageVO) {
         logger.info("find food board in ReadDiaryService : {} ", UserContextHolder.getContext().getCorrelationId());
 
+        List<Predicate> predicates = new ArrayList<>();
+
+        //1. id에 해당하는 작성자이고
+        predicates.add(qDocument.writerId.eq(Long.parseLong(writerId)));
+
+        //2. 작성 기간 안에 쓰여진
+        addPredicateForTimeSpan(predicates, foodPageVO);
+
+        //3. 일지를 모두 찾는다.
+        List<DiabetesDiaryDocument> diaryDocumentList = (List<DiabetesDiaryDocument>) diaryDocumentRepository.findAll(ExpressionUtils.allOf(predicates));
+
+        //4. 식사 혈당 필터 조건이 있다면 필터링 한다. 그리고 필터링한 식단과 음식을 모아 DTO로 만든다.
+        List<FoodBoardDTO> foodBoardDTOList = filterWithEqualitySignAndBloodSugar(diaryDocumentList, foodPageVO.getEnumOfSign(), foodPageVO.getBloodSugar());
+
+        //5. DTO를 조건 순으로 정렬한다.
+        foodBoardDTOList.sort(Comparator
+                .comparing(FoodBoardDTO::getBloodSugar, Comparator.reverseOrder())
+                .thenComparing(FoodBoardDTO::getWrittenTime, Comparator.reverseOrder())
+                .thenComparing(FoodBoardDTO::getFoodName, Comparator.naturalOrder()));
+
+        //6. 5.의 결과를 페이징한다.
+
         return null;
     }
 
-    private BooleanBuilder decideEqualitySignOfBloodSugar(InequalitySign sign, int bloodSugar) {
-        BooleanBuilder booleanBuilder = new BooleanBuilder();
 
-        switch (sign) {
-            case GREATER:
-                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.gt(bloodSugar));
-                break;
+    private void addPredicateForTimeSpan(List<Predicate> predicates, FoodPageVO vo) {
+        LocalDateTime startDate;
 
-            case LESSER:
-                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.lt(bloodSugar));
-                break;
+        LocalDateTime endDate;
 
-            case EQUAL:
-                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.eq(bloodSugar));
-                break;
+        try {
+            startDate = vo.convertStartDate().orElseThrow(IllegalArgumentException::new);
 
-            case GREAT_OR_EQUAL:
-                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.goe(bloodSugar));
-                break;
-
-            case LESSER_OR_EQUAL:
-                booleanBuilder.and(QDietDocument.dietDocument.bloodSugar.loe(bloodSugar));
-                break;
+            endDate = vo.convertEndDate().orElseThrow(IllegalArgumentException::new);
+        } catch (IllegalArgumentException e) {
+            logger.info("It is invalid date format... So, this predicate should be ignored...");
+            return;
         }
 
-        return booleanBuilder;
+        if (isStartDateEqualOrBeforeEndDate(startDate, endDate)) {
+            predicates.add(qDocument.writtenTime.between(startDate, endDate));
+        }
+    }
+
+    private List<FoodBoardDTO> filterWithEqualitySignAndBloodSugar(List<DiabetesDiaryDocument> diaryDocumentList, InequalitySign sign, int bloodSugar) {
+
+        List<FoodBoardDTO> dtoList = new ArrayList<>();
+
+        for (DiabetesDiaryDocument diaryDocument : diaryDocumentList) {
+            for (DietDocument dietDocument : diaryDocument.getDietList()) {
+                if (!isRightCondition(dietDocument, sign, bloodSugar)) {
+                    continue;
+                }
+                for (FoodDocument foodDocument : dietDocument.getFoodList()) {
+                    dtoList.add(new FoodBoardDTO(foodDocument.getFoodName(), dietDocument.getBloodSugar(), diaryDocument.getWrittenTime(), diaryDocument.getDiaryId()));
+                }
+            }
+        }
+
+        return dtoList;
+    }
+
+    private boolean isRightCondition(DietDocument dietDocument, InequalitySign sign, int bloodSugar) {
+        switch (sign) {
+            case GREATER:
+                return dietDocument.getBloodSugar() > bloodSugar;
+
+            case LESSER:
+                return dietDocument.getBloodSugar() < bloodSugar;
+
+            case EQUAL:
+                return dietDocument.getBloodSugar() == bloodSugar;
+
+            case GREAT_OR_EQUAL:
+                return dietDocument.getBloodSugar() >= bloodSugar;
+
+            case LESSER_OR_EQUAL:
+                return dietDocument.getBloodSugar() <= bloodSugar;
+        }
+        // 조건이 없으면 다 dto로 담아야 하므로 true를 반환.
+        return true;
     }
 }
